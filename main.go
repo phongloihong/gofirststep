03643280e160ecead5fac598cbcd5e1116ec4f22
phongloihong <phongloi1707@gmail.com>
2018-04-08 21:50:27 +0700

    Deploy with app engine

--- main.go
+++ main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"io"
 	"net/http"
+
+	"google.golang.org/appengine"
 )
 
 func main() {
 	http.HandleFunc("/", serveImage)
 	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./"))))
-	http.ListenAndServe(":8080", nil)
+	appengine.Main()
 }
 
 func serveImage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	io.WriteString(w, `<img src="/resources/go-logo.png">`)
+	io.WriteString(w, `<img src="/resources/go-logo.png"> <h1>Hello man</h1>`)
 }
